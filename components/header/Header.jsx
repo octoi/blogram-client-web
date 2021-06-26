823ee91522295cf449f640e55a62eb51c6e7111d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-26 13:20:38 +0530

    menu

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,12 +1,13 @@
-import { Flex, Text, Input } from '@chakra-ui/react';
+import { Flex, Text, Input, Button } from '@chakra-ui/react';
 import { InputGroup, InputLeftElement } from '@chakra-ui/react';
-import { SearchIcon } from '@chakra-ui/icons';
+import { SearchIcon, ChevronDownIcon } from '@chakra-ui/icons';
+import { Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react';
 
 export default function Header() {
 	return (
 		<Flex mt={5} justifyContent="space-evenly" width="100%">
 			<Text fontSize="2xl">Blogram</Text>
-			<InputGroup ml={2}>
+			<InputGroup ml={2} mr={2}>
 				<InputLeftElement pointerEvents="none">
 					<SearchIcon color="gray.300" />
 				</InputLeftElement>
@@ -15,6 +16,18 @@ export default function Header() {
 					variant="filled"
 				/>
 			</InputGroup>
+			<div>
+				<Menu>
+					<MenuButton as={Button}>
+						username
+					</MenuButton>
+					<MenuList>
+						<MenuItem>Profile</MenuItem>
+						<MenuItem>New Blog</MenuItem>
+						<MenuItem>Settings</MenuItem>
+					</MenuList>
+				</Menu>
+			</div>
 		</Flex>
 	)
 }
