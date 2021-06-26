356be782207ca6e7da80ef8e1d31737274a45bef
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-26 13:10:35 +0530

    search bar

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,9 +1,20 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Text, Input } from '@chakra-ui/react';
+import { InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
 
 export default function Header() {
 	return (
-		<Flex mt={5} justifyContent="space-between">
+		<Flex mt={5} justifyContent="space-evenly" width="100%">
 			<Text fontSize="2xl">Blogram</Text>
+			<InputGroup ml={2}>
+				<InputLeftElement pointerEvents="none">
+					<SearchIcon color="gray.300" />
+				</InputLeftElement>
+				<Input
+					placeholder="Search for blogs ..."
+					variant="filled"
+				/>
+			</InputGroup>
 		</Flex>
 	)
 }
