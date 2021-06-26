17999ff330f0d67ee08c04be4885615de2bbd938
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-26 13:54:38 +0530

    states

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,30 @@
+import React, { useState } from 'react';
 import { Flex, Button, Heading, Input, Link } from '@chakra-ui/react';
 import { LoginStyledFlex } from '../styles/styledComponents';
 
-export default function login() {
+export default function Login() {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+
 	return (
 		<Flex height="80vh" justifyContent="center" alignItems="center">
 			<LoginStyledFlex background="gray.700" p={12} direction="column">
 				<Heading>Log In</Heading>
-				<Input mt={5} placeholder="username" variant="filled" />
-				<Input mt={5} placeholder="••••••••" variant="filled" />
+				<Input
+					mt={5}
+					placeholder="username"
+					variant="filled"
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<Input
+					mt={5}
+					placeholder="••••••••"
+					variant="filled"
+					value={password}
+					type="password"
+					onChange={(e) => setPassword(e.target.value)}
+				/>
 				<Button mt={5} colorScheme="teal" width="100%">Log In</Button>
 				<Link mt={2}>New to blogram ?? Register</Link>
 			</LoginStyledFlex>
