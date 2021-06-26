6b95ab81a00cc408c1027f47d8498558fb56184e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-26 12:58:12 +0530

    used header

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
 import ChakraWrap from '../utils/ChakraWrap';
+import Header from '../components/header/Header';
 import { AppContext } from '../context/AppContext';
 
 function MyApp({ Component, pageProps }) {
     return (
         <AppContext>
             <ChakraWrap>
+                <Header />
                 <Component {...pageProps} />
             </ChakraWrap>
         </AppContext>
