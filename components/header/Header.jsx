c7f2834acaff6bcd6c946d58cea2b26a427356bf
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-26 12:59:04 +0530

    created title

---
+++ components/header/Header.jsx
@@ -0,0 +1,9 @@
+import { Flex, Text } from '@chakra-ui/react';
+
+export default function Header() {
+	return (
+		<Flex mt={5} justifyContent="space-between">
+			<Text fontSize="2xl">Blogram</Text>
+		</Flex>
+	)
+}
