e9527302cfc674c0b69ea6ba5693bdf41a9a6245
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-26 13:51:51 +0530

    changed link text

--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,16 @@
 import { Flex, Button, Heading, Input, Link } from '@chakra-ui/react';
+import { LoginStyledFlex } from '../styles/styledComponents';
 
 export default function login() {
 	return (
 		<Flex height="80vh" justifyContent="center" alignItems="center">
-			<Flex background="gray.700" p={12} direction="column">
+			<LoginStyledFlex background="gray.700" p={12} direction="column">
 				<Heading>Log In</Heading>
 				<Input mt={5} placeholder="username" variant="filled" />
 				<Input mt={5} placeholder="••••••••" variant="filled" />
 				<Button mt={5} colorScheme="teal" width="100%">Log In</Button>
-				<Link mt={2}>Don't have an account ?? Register</Link>
-			</Flex>
+				<Link mt={2}>New to blogram ?? Register</Link>
+			</LoginStyledFlex>
 		</Flex>
 	)
 }
