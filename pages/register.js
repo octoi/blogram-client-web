5fba42ee4762a6c21427088aa5d1f29af49649c8
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-27 14:21:07 +0530

    used auth denied

--- pages/register.js
+++ pages/register.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import useAppContext from '../hooks/useAppContext';
+import AuthDenied from '../utils/AuthDenied';
 import { useRouter } from 'next/router';
 import { Flex, Button, Heading, Input, Link } from '@chakra-ui/react';
 import { registerUser } from '../api/authentication';
@@ -23,40 +24,42 @@ export default function Register() {
 	}
 
 	return (
-		<Flex height="80vh" justifyContent="center" alignItems="center">
-			<Flex maxW="80%" minW="30%" background="gray.700" p={12} direction="column">
-				<Heading>Register</Heading>
-				<Input
-					mt={5}
-					placeholder="name"
-					variant="filled"
-					value={name}
-					onChange={(e) => setName(e.target.value)}
-				/>
-				<Input
-					mt={5}
-					placeholder="username"
-					variant="filled"
-					value={username}
-					onChange={(e) => setUsername(e.target.value)}
-				/>
-				<Input
-					mt={5}
-					placeholder="••••••••"
-					variant="filled"
-					value={password}
-					type="password"
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-				<Button
-					mt={5}
-					colorScheme="teal"
-					width="100%"
-					onClick={register}
-					disabled={username.trim().length === 0 || name.trim().length === 0 || password.trim().length === 0}
-				>Register</Button>
-				<Link mt={2} href='/login'>Already in blogram ?? Login</Link>
+		<AuthDenied>
+			<Flex height="80vh" justifyContent="center" alignItems="center">
+				<Flex maxW="80%" minW="30%" background="gray.700" p={12} direction="column">
+					<Heading>Register</Heading>
+					<Input
+						mt={5}
+						placeholder="name"
+						variant="filled"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					/>
+					<Input
+						mt={5}
+						placeholder="username"
+						variant="filled"
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+					<Input
+						mt={5}
+						placeholder="••••••••"
+						variant="filled"
+						value={password}
+						type="password"
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<Button
+						mt={5}
+						colorScheme="teal"
+						width="100%"
+						onClick={register}
+						disabled={username.trim().length === 0 || name.trim().length === 0 || password.trim().length === 0}
+					>Register</Button>
+					<Link mt={2} href='/login'>Already in blogram ?? Login</Link>
+				</Flex>
 			</Flex>
-		</Flex>
+		</AuthDenied>
 	)
 }
