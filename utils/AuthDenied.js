d81ed6b3a76dd610890fa1b6311a8084d343b0e7
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-27 14:20:55 +0530

    auth denied

---
+++ utils/AuthDenied.js
@@ -0,0 +1,18 @@
+import useAppContext from '../hooks/useAppContext';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function AuthProtected({ children }) {
+	const { user } = useAppContext();
+	const router = useRouter();
+
+	useEffect(() => {
+		if (user) router.push('/')
+	}, [children, user, router])
+
+	return (
+		<div>
+			{children}
+		</div>
+	)
+}
