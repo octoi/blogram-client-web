897621d9552a3d3718bf2b9c3c2f9f987a690a72
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-27 15:43:06 +0530

    ...

--- styles/styledComponents.js
+++ styles/styledComponents.js
@@ -5,6 +5,7 @@ import { Flex } from '@chakra-ui/react';
 export const HomeBlogContainer = styled(Flex)`
 	cursor: pointer;
 	transition: 0.3s;
+	flex-direction: column;
 
 	&:hover {
 		background: var(--chakra-colors-whiteAlpha-200);
