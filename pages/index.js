bf678982accb293dfd0071f3ff10b3a75a164f89
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-27 14:40:19 +0530

    fetching all blogs

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import useAppContext from '../hooks/useAppContext';
+import { fetchAllBlogs } from '../api/blog';
+
 export default function Home() {
+	const [blogs, setBlogs] = useState();
+	const { showToast } = useAppContext();
+
+	useEffect(() => {
+		fetchAllBlogs().then((blogs) => {
+			setBlogs(blogs);
+		}).catch(err => {
+			showToast({ title: err });
+		})
+	}, [showToast])
+
 	return (
 		<div>
-
 		</div>
 	)
 }
