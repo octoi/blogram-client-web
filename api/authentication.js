284fae65e1046c452d583a0ec122f623ea5145f7
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-27 14:02:41 +0530

    login function

--- api/authentication.js
+++ api/authentication.js
@@ -2,18 +2,27 @@ import axios from 'axios';
 import { serverUrl } from '../utils/constants';
 
 const registerRoute = `${serverUrl}/user/register`;
+const loginRoute = `${serverUrl}/user/login`;
 
 export function registerUser(userData) {
 	return new Promise((resolve, reject) => {
-		axios.post(registerRoute, userData)
+		axios.post(registerRoute, userData).then(res => {
+			const data = res.data;
+			if (data?.status === 200) {
+				resolve(data?.message)
+			} else {
+				reject(data?.message);
+			}
+		}).catch(() => reject("Couldn't request to server !"));
+	});
+}
 
-			.then(res => {
-
-				const data = res.data;
-				if (data?.status === 200) resolve(data?.message)
-				else reject(data?.message);
-
-			}).catch(() => reject('Could request to server !'));
-
+export function loginUser(userData) {
+	return new Promise((resolve, reject) => {
+		axios.post(loginRoute, userData).then(res => {
+			const data = res.data;
+			if (data?.status === 200) resolve(data?.message)
+			else reject(data?.message);
+		}).catch(() => reject("Couldn't request to server !"));
 	});
 }
