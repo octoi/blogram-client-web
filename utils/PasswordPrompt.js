ef44078198ac827ebc69f2bc80bbec4367521be9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-28 13:47:54 +0530

    password propmt

---
+++ utils/PasswordPrompt.js
@@ -0,0 +1,47 @@
+import React, { useRef, useState } from 'react';
+import {
+	AlertDialog,
+	AlertDialogBody,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogContent,
+	AlertDialogOverlay,
+	Button,
+	Input
+} from "@chakra-ui/react"
+
+export default function PasswordPrompt({ isOpen, onClose, setValue }) {
+	const [password, setPassword] = useState('');
+	const cancelRef = useRef();
+
+	const handleBtnPress = () => {
+		setValue(password)
+	}
+
+	return (
+		<AlertDialog
+			isOpen={isOpen}
+			leastDestructiveRef={cancelRef}
+			onClose={onClose}
+			isCentered
+		>
+			<AlertDialogOverlay>
+				<AlertDialogContent>
+					<AlertDialogHeader fontSize="lg" fontWeight="bold">Enter Password</AlertDialogHeader>
+					<AlertDialogBody>
+						<Input
+							placeholder="••••••••"
+							type="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+					</AlertDialogBody>
+					<AlertDialogFooter>
+						<Button ref={cancelRef} onClick={onClose}>Cancel</Button>
+						<Button colorScheme="teal" disabled={password.length === 0} onClick={handleBtnPress} ml={3}>Confirm</Button>
+					</AlertDialogFooter>
+				</AlertDialogContent>
+			</AlertDialogOverlay>
+		</AlertDialog>
+	)
+}
