bb295daf0025f0437f10dffb10a8241be1c2d863
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-28 14:09:30 +0530

    styled

--- components/home/Blog.jsx
+++ components/home/Blog.jsx
@@ -19,7 +19,7 @@ export default function Blog({ blog }) {
 
 
 	return (
-		<HomeBlogContainer mt={4} onClick={() => router.push(`/blog/${blog?._id}`)} background="gray.700" borderRadius="12" p={7}>
+		<HomeBlogContainer mt={4} onClick={() => router.push(`/blog/${blog?._id}`)} borderRadius="12" p={7}>
 			<Text fontSize="3xl" fontWeight="medium">{blog?.title}</Text>
 			<Text mt={2} opacity="0.8" fontSize="xl">{moment(parseInt(blog?.createdAt)).fromNow()}</Text>
 			<Text fontSize="xl" opacity="0.5" mt={3}>{`${blog?.blog.substr(1, 200)} ...`}</Text>
