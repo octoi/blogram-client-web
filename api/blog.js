7930218ce5ecc11ea80ef8f07ba2c5b4f6563d6b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-28 14:24:46 +0530

    fetching single blog

--- api/blog.js
+++ api/blog.js
@@ -6,12 +6,12 @@ const blogRoute = `${serverUrl}/blog`;
 export function fetchAllBlogs() {
 	return new Promise((resolve, reject) => {
 		axios.get(blogRoute).then(res => {
+
 			const data = res.data;
-			if (data?.status === 200) {
-				resolve(data?.message);
-			} else {
-				reject(data?.message);
-			}
+
+			if (data?.status === 200) { resolve(data?.message); }
+			else { reject(data?.message); }
+
 		}).catch(() => reject("Failed to fetch data"))
 	});
 }
@@ -21,12 +21,27 @@ export function createBlog(blogData) {
 		const route = `${blogRoute}/new`;
 
 		axios.post(route, blogData).then(res => {
+
 			const data = res.data;
-			if (data?.status === 200) {
-				resolve(data?.message);
-			} else {
-				reject(data?.message);
-			}
+
+			if (data?.status === 200) { resolve(data?.message); }
+			else { reject(data?.message); }
+
 		}).catch(() => reject('Failed to create blog :('));
+	});
+}
+
+export function fetchOneBlog(id) {
+	return new Promise((resolve, reject) => {
+		const route = `${blogRoute}/${id}`;
+
+		axios.get(route).then(res => {
+
+			const data = res.data;
+
+			if (data?.status === 200) { resolve(data?.message) }
+			else { reject(data?.message); }
+
+		}).catch(() => reject("Failed to fetch data"))
 	});
 }
