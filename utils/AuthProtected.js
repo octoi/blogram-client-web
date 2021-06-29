8b224b71cd53105741e7b74b782502f5e9b7b9bb
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-29 14:05:09 +0530

    better check

--- utils/AuthProtected.js
+++ utils/AuthProtected.js
@@ -6,29 +6,25 @@ import { Flex, Spinner } from '@chakra-ui/react';
 export default function AuthProtected({ children }) {
 	const { user, showToast } = useAppContext();
 	const [loading, setLoading] = useState(false);
-	const [count, setCount] = useState(0);
 	const router = useRouter();
 
 
 
 	useEffect(() => {
 		setLoading(true);
-		setTimeout(() => {
-			const checkForUser = () => {
+		if (!user) {
+			setTimeout(() => {
 				if (!user) {
-					if (count !== 1) {
-						checkForUser();
-						setCount(1);
-						return;
-					}
 					showToast({ title: 'Permission denied', description: 'Please login to continue' });
 					router.push('/login');
+				} else {
+					setLoading(false);
 				}
-				setLoading(false);
-			}
-			checkForUser();
-		}, 1000)
-	}, [children, user, router, showToast, count])
+			}, 2000)
+		} else {
+			setLoading(false);
+		}
+	}, [children, user, router, showToast])
 
 	return (
 		<div>
